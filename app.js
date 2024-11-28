021fa814b3048643bb198af8b5e4d75028e41a15
ozge <karabayozge@gmail.com>
2024-11-28 23:42:34 +0300

    template engine

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express=require("express");
+const path=require("path")
+const ejs=require("ejs");
 const app=express();
+app.use(express.static("public"));
+app.set("view engine","ejs")
 app.get("/",(req,res)=>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog);
+    res.render("index");
+})
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
+app.get("/add_post",(req,res)=>{
+    res.render("add_post");
+})
+app.get("/post",(req,res)=>{
+    res.render("post");
 })
 const port=3000;
 
